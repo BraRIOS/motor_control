862e5d30cf8dcf9f5edd58ffc26630356d9788e6
BraRIOS <braian.rios@ing.austral.edu.ar>
2023-11-10 14:15:39 -0300

    Websocket and file system ESP32 WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,144 @@
-/*
- *      Project Motor_control
- *      Using a switch to control a motor
- *
- *      main.cpp
- */
+// Import required libraries
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include "webpage.h"
 
-#include <Arduino.h>
+// Replace with your network credentials
+const char* ssid = "UA-Alumnos";
+const char* password = "41umn05WLC";
 
-/*
- *     Definiciones
- */
-#define MOTOR           23
-#define SWITCH          22
+bool motorState = 0;
+const int motorPin = 23;
+const int switchPin = 22;
 
-/*
- *  Public functions
- */
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
-
-void
-setup(void)
-{
-    Serial.begin(SERIAL_BAUD);
-    pinMode(MOTOR, OUTPUT);
-    pinMode(SWITCH, INPUT_PULLUP);
+void notifyClients() {
+  ws.textAll(String(motorState));
 }
 
-void
-loop(void)
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char*)data, "toggle") == 0) {
+      motorState = true;
+      notifyClients();
+    }
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:
+      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+      break;
+  }
+}
+
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    if (motorState){
+      return "ON";
+    }
+    else{
+      return "OFF";
+    }
+  }
+  return String();
+}
+
+static int verify_half_turn(void)
 {
-    int digital_value = digitalRead(SWITCH);
-    Serial.println(digital_value);
-    if(digital_value == HIGH)
-        digitalWrite(MOTOR, HIGH);
-    else
-        digitalWrite(MOTOR, LOW);
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        if (digitalRead(switchPin) == 0)
+        {
+            return 1;
+        }
+        delay(100);
+    }
+    return 0;
+}
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(SERIAL_BAUD);
+
+  //innitialize pins
+  pinMode(motorPin, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP);
+  digitalWrite(motorPin, LOW);
+
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  } 
+  
+  File file = SPIFFS.open("/index.html");
+  if(!file){
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  
+  Serial.println("File Content:");
+  while(file.available() - (file.size()-20)){
+    Serial.write(file.read());
+  }
+  file.close();
+    
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi..");
+    }
+
+    // Print ESP Local IP Address
+    Serial.println(WiFi.localIP());
+
+    initWebSocket();
+
+    // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", index_html, processor);
+    });
+
+    // Start server
+    server.begin();
+}
+
+void loop() {
+  ws.cleanupClients();
+  if(motorState){
+    digitalWrite(motorPin, motorState);
+    if (verify_half_turn() == 1)
+    {
+      motorState = !motorState;
+      digitalWrite(motorPin, motorState);
+      notifyClients();
+    }
+  }
 }
